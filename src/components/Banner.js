7ae3f43ce3b15d187fa08ed16fd594d264a771be
Geetha Karthik <reachgitak@gmail.com>
2023-07-24 21:19:47 -0500

    Update the code

---
+++ src/components/Banner.js
@@ -0,0 +1,8 @@
+function Banner(){
+    return(
+       <header>Orbit Report<p>Click on the buttons to see the satellites in that orbit type</p></header>
+       );
+
+};
+
+export default Banner;
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,18 +1,32 @@
 const Table = ({ sat }) => {
+  
+  
   return (
-      <table>
-       <thead>
-        <tr>
-          <th>Header TBD</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-          <td>Row Data TBD</td>
-        </tr>
-        </tbody>
-      </table>
-  );
+    <table>
+     <thead>
+      <tr>
+        <th>Name</th>
+        <th>Type of Satellite</th>
+        <th>Launch Date</th>
+        <th>Status</th>
+         </tr>
+      </thead>
+      {sat.map((data, id) =>{
+        return (
+          <tbody>
+          <tr key={id}>
+          <td>{data.name}</td>
+          <td>{data.type}</td>
+          <td>{data.launchDate}</td>
+          <td>{data.operational? "active":"inactive"}</td>
+          </tr>
+          </tbody> 
+        )
+      })}
+      
+    </table>
+);
+  
 };
 
 export default Table;
